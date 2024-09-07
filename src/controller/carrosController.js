65e76ff2f0792ab55fd19de7a819baeb59297efc
debrinhauwu <deborassantos880@gmail.com>
2024-09-07 17:19:49 -0300

    leonardo viado

--- src/controller/carrosController.js
+++ src/controller/carrosController.js
@@ -1,75 +1,89 @@
 import * as db from '../repository/carrosRepository.js';
-
+import multer from 'multer';
 import { Router } from 'express';
+
 const endpoints = Router();
 
 endpoints.get('/carro/', async (req, resp) => {
   try {
     let registros = await db.consultarCarro();
-    resp.send(registros)
+    resp.send(registros);
   } catch (err) {
     resp.status(400).send({
       erro: err.message
-    })
+    });
   }
-})
+});
 
-endpoints.post('/carro/', async (req, resp)=> {
+endpoints.post('/carro/', async (req, resp) => {
   try {
     let carro = req.body;
     let id = await db.inserirCarro(carro);
 
     resp.send({
       novoId: id
-    })
+    });
   } catch (err) {
     resp.status(400).send({
       erro: err.message
-    })
+    });
   }
-})
+});
 
-
-endpoints.put('/carro/:id', async(req, resp) => {
+endpoints.put('/carro/:id', async (req, resp) => {
   try {
     let id = req.params.id;
-    let carro = req.body
-    
+    let carro = req.body;
+
     let linhasAfetadas = await db.alterarCarro(id, carro);
-    if(linhasAfetadas == 1){
+    if (linhasAfetadas == 1) {
       resp.send();
-    }
-    else {
+    } else {
       resp.status(404).send({
         erro: 'Nenhuma linha alterada'
-      })
+      });
     }
-    
   } catch (err) {
     resp.status(400).send({
       erro: err.message
-    })
+    });
   }
-})
+});
 
 endpoints.delete('/carro/:id', async (req, resp) => {
   try {
     let id = req.params.id;
 
     let linhasAfetadas = await db.removerCarro(id);
-    if(linhasAfetadas == 1){
+    if (linhasAfetadas == 1) {
       resp.send();
-    }
-    else {
+    } else {
       resp.status(404).send({
-        erro: 'Nenhuma linha alterada'
-      })
+        erro: 'Nenhuma linha removida'
+      });
     }
   } catch (err) {
     resp.status(400).send({
       erro: err.message
-    })
+    });
   }
-})
+});
+
+let uploadFotos = multer({ dest: './storage/carrosFotos' });
+
+endpoints.put('/carro/:id/imagem', uploadFotos.single('imagem'), async (req, resp) => {
+  try {
+    let id = req.params.id;
+    let caminho = req.file.path;
+
+    await db.alterarImagem(id, caminho);
+
+    resp.status(204).send(); 
+  } catch (err) {
+    resp.status(400).send({
+      erro: err.message
+    });
+  }
+});
 
 export default endpoints;
--- src/repository/carrosRepository.js
+++ src/repository/carrosRepository.js
@@ -1,49 +1,50 @@
-import con from "./connection.js"
+import con from './connection.js';
 
 export async function inserirCarro(carro) {
   const comando = `
     insert into tb_carro (ds_marca, ds_modelo, nr_ano, vl_preco, img_carro, dt_inclusao)
-							values (?, ?, ?, ?, ?, ?);
+    values (?, ?, ?, ?, ?, ?);
   `;
 
-  let resposta = await con.query(comando, carro.marca, carro.modelo, carro.ano, carro.preco, carro.img, carro.inclusao)
-  let info = resposta[0]
+  let resposta = await con.query(comando, [carro.marca, carro.modelo, carro.ano, carro.preco, carro.img, carro.inclusao]);
+  let info = resposta[0];
 
   return info.insertId;
 }
 
 export async function consultarCarro() {
   const comando = `
-      select id_carro               id,
-       ds_marca                    marca,
-       ds_modelo                   modelo,
-       nr_ano                      ano,
-       vl_preco                    preço,
-       img_carro                   image,
-       dt_inclusao                 inclusão,
-      from tb_carro
+    select 
+      id_carro               id,
+      ds_marca               marca,
+      ds_modelo              modelo,
+      nr_ano                 ano,
+      vl_preco               preço,
+      img_carro              image,
+      dt_inclusao            inclusão
+    from tb_carro;
   `;
 
-  let resposta = await con.query(comando)
-  let registros = resposta[0]
+  let resposta = await con.query(comando);
+  let registros = resposta[0];
 
   return registros;
 }
 
 export async function alterarCarro(id, carro) {
   const comando = `
-      update tb_carro 
-        set ds_marca = ?,
+    update tb_carro 
+      set ds_marca = ?,
           ds_modelo = ?,
           nr_ano = ?,
           vl_preco = ?,
           img_carro = ?,
           dt_inclusao = ?
-        where id_carro = ?;
-  `
-  
-  let resposta = await con.query(comando, [carro.marca, carro.modelo, carro.ano, carro.preco, carro.img, carro.inclusao, id])
-  let info = resposta[0]
+    where id_carro = ?;
+  `;
+
+  let resposta = await con.query(comando, [carro.marca, carro.modelo, carro.ano, carro.preco, carro.img, carro.inclusao, id]);
+  let info = resposta[0];
 
   return info.affectedRows;
 }
@@ -51,26 +52,24 @@ export async function alterarCarro(id, carro) {
 export async function removerCarro(id) {
   const comando = `
     delete from tb_carro 
-      where id_carro = ?;
-  `
+    where id_carro = ?;
+  `;
 
   let resposta = await con.query(comando, [id]);
-  let info = resposta[0]
+  let info = resposta[0];
 
   return info.affectedRows;
 }
-
 
 export async function alterarImagem(id, caminho) {
   const comando = ` 
     update tb_carro 
       set img_carro = ?
-    where id_carro = ?
-  `
+    where id_carro = ?;
+  `;
 
-  let resposta = await con.query(comando, [id, caminho])
-  let info = resposta[0]
+  let resposta = await con.query(comando, [caminho, id]); 
+  let info = resposta[0];
 
-  let linhasAfetadas = info.affectedRows
-  return linhasAfetadas
+  return info.affectedRows;
 }
